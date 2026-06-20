13a161314d891fa42c241cb45c35710abd4d7788
Rickshii <rickshithas33@gmail.com>
2026-06-20 19:12:06 +0530

    chore: add Razorpay configurations to Settings

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -11,6 +11,10 @@ class Settings(BaseSettings):
     USE_SQLITE_FALLBACK: bool = True
     SQLITE_DATABASE_URL: str = "sqlite:///./ai_marketing.db"
 
+    # Razorpay configurations
+    RAZORPAY_KEY_ID: str = ""
+    RAZORPAY_KEY_SECRET: str = ""
+
     class Config:
         env_file = ".env"
         case_sensitive = True
